7a4b51a0c0cdf8817bcc1674287561fafc19b512
zhangcong <congba520@163.com>
2020-07-01 21:33:44 +0800

    test pom

---
+++ src/main/java/com/mycompany/app/domain/UserDO.java
@@ -0,0 +1,17 @@
+package com.mycompany.app.domain;
+
+import lombok.Data;
+
+/**
+ * @program: simple-java-maven-app
+ * @create: 2020-07-01 21:25
+ * @author: zhangcong
+ * @description:
+ **/
+@Data
+public class UserDO {
+    private String username;
+    private String pwd;
+
+
+}
